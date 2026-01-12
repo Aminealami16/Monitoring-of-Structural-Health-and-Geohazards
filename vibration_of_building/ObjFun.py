d84147a299c90cb3dda7b8eac23b8388127ecfbd
Nigel Wiesemann <n.wiesemann@student.tudelft.nl>
2026-01-12 15:48:09 +0100

    dd

--- vibration_of_building/ObjFun.py
+++ vibration_of_building/ObjFun.py
@@ -54,7 +54,7 @@ def ObjFun(x, sclx1, sclx2):
     #---------------------------------------------------------------------------------------------------------#
     
     # Define a maxium number of modes to be used
-    maxnomod = 3
+    maxnomod = 4
     #print(min(maxnomod, nmatch))
     
     T1 = np.zeros((min(maxnomod, nmatch),))        # First term of the cost function
